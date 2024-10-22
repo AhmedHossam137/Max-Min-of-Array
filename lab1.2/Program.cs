ff565c50d096eaa5382ffbe5657f43ed8df35083
Ahmed <Ahmed@AHMEDHOSSAM>
2024-10-22 17:59:25 +0300

    Add project files.

---
+++ lab1.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"Enter the number for index {i}:");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int max = getMax(arr);
+        int min = getMin(arr);
+
+        Console.WriteLine($"Maximum value: {max}");
+        Console.WriteLine($"Minimum value: {min}");
+    }
+
+    static int getMax(int[] arr)
+    {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    static int getMin(int[] arr)
+    {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+}
